e02157d43c02074937ef47d534785e18db9eec59
Farrel Andhika Rizky Putra <farrelarp@students.undip.ac.id>
2022-08-31 13:02:00 +0700

    first commit

---
+++ questions.php
@@ -0,0 +1,121 @@
+<?php
+global $questionId, $userAnswer;
+$questionId = 1;
+$questions = [
+    [
+        'question' => 'What is the capital of Australia?',
+        'answers' => [
+            [
+                'id' => 1,
+                'answer' => 'Canberra'
+            ],
+            [
+                'id' => 2,
+                'answer' => 'Sydney'
+            ],
+            [
+                'id' => 3,
+                'answer' => 'Melbourne'
+            ],
+            [
+                'id' => 4,
+                'answer' => 'Brisbane'
+            ]
+        ],
+        'trueAnswer' => 1,
+        'userAnswer' => 0,
+    ],
+    [
+        'question' => 'What is the capital of New Zealand?',
+        'answers' => [
+            [
+                'id' => 1,
+                'answer' => 'Wellington'
+            ],
+            [
+                'id' => 2,
+                'answer' => 'Auckland'
+            ],
+            [
+                'id' => 3,
+                'answer' => 'Christchurch'
+            ],
+            [
+                'id' => 4,
+                'answer' => 'Dunedin'
+            ]
+        ],
+        'trueAnswer' => 1,
+        'userAnswer' => 0
+    ],
+    [
+        'question' => 'What is the capital of Indonesia?',
+        'answers' => [
+            [
+                'id' => 1,
+                'answer' => 'Jakarta'
+            ],
+            [
+                'id' => 2,
+                'answer' => 'Sydney'
+            ],
+            [
+                'id' => 3,
+                'answer' => 'Melbourne'
+            ],
+            [
+                'id' => 4,
+                'answer' => 'Brisbane'
+            ]
+        ],  
+        'trueAnswer' => 1,
+        'userAnswer' => 0
+    ],
+    [
+        'question' => 'What is the capital of Malaysia?',
+        'answers' => [
+            [
+                'id' => 1,
+                'answer' => 'Kuala Lumpur'
+            ],
+            [
+                'id' => 2,
+                'answer' => 'Auckland'
+            ],
+            [
+                'id' => 3,
+                'answer' => 'Christchurch'
+            ],
+            [
+                'id' => 4,
+                'answer' => 'Dunedin'
+            ]
+        ],
+        'trueAnswer' => 1,
+        'userAnswer' => 0
+    ],
+    [
+        'question' => 'What is the capital of Phillipines?',
+        'answers' => [
+            [
+                'id' => 1,
+                'answer' => 'Manila'
+            ],
+            [
+                'id' => 2,
+                'answer' => 'Auckland'
+            ],
+            [
+                'id' => 3,
+                'answer' => 'Christchurch'
+            ],
+            [
+                'id' => 4,
+                'answer' => 'Dunedin'
+            ]
+        ],
+        'trueAnswer' => 1,
+        'userAnswer' => 0
+    ]
+    ];
+?>
