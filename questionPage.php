53d7920c5fb7304444fb5036b7458fadb261870d
Farrel Andhika Rizky Putra <farrelarp@students.undip.ac.id>
2022-09-01 11:58:12 +0700

    update questionPage

--- questionPage.php
+++ questionPage.php
@@ -39,7 +39,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quiz</title>
+    <title>Quiz - Soal <?=$questionNum?> dari <?=count($questions)?></title>
 </head>
 <body>
     <form action="questionPage.php" method="GET">
